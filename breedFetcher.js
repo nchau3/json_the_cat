52373de9c71716806e390b4ff628d07279611e4b
Nicholas <nc_chau@hotmail.com>
2022-09-01 18:39:02 +0000

    iadded testing via mocha chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,16 @@
-const breed = process.argv.slice(2);
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) console.log("REQUEST FAILED ", error);
-  if (body === '[]') return console.log("Breed not found");
-  const data = JSON.parse(body);
-  return console.log(data);
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error);
+    }
+    if (body === '[]') {
+      return callback("Breed not found");
+    }
+    const data = JSON.parse(body);
+    return callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
